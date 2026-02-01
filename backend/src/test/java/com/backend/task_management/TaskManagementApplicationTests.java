7262fe06e5922552f69acefd0b8a4027408b4590
Akmal <m.akmalalavi@gmail.com>
2026-02-01 22:29:59 +0530

    refactor: remove test profile properties and update test configuration

--- backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
+++ backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
@@ -1,11 +1,12 @@
 package com.backend.task_management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 class TaskManagementApplicationTests {
 
 	@Test
