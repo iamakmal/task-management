caf9f9287d3dfbf373f4c947e6189e3ecd19d770
Akmal <m.akmalalavi@gmail.com>
2026-02-02 14:16:20 +0530

    refactor: simplify test class by removing unused MockitoBeans and imports

--- backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
+++ backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
@@ -2,19 +2,9 @@ package com.backend.task_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import com.backend.task_management.repository.TaskRepository;
-import com.backend.task_management.service.TaskService;
-
-@SpringBootTest()
+@SpringBootTest
 class TaskManagementApplicationTests {
-
-	@MockitoBean
-	private TaskRepository taskRepository;
-
-	@MockitoBean
-	private TaskService taskService;
 
 	@Test
 	void contextLoads() {
