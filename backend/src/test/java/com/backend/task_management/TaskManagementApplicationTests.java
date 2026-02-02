54c398389ec7a69fb1a86c1ffe711b2393a55cbe
Akmal <m.akmalalavi@gmail.com>
2026-02-02 13:41:22 +0530

    refactor: update test configuration by removing unused properties and adding MockitoBeans

--- backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
+++ backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
@@ -2,11 +2,19 @@ package com.backend.task_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = {
-		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-})
+import com.backend.task_management.repository.TaskRepository;
+import com.backend.task_management.service.TaskService;
+
+@SpringBootTest()
 class TaskManagementApplicationTests {
+
+	@MockitoBean
+	private TaskRepository taskRepository;
+
+	@MockitoBean
+	private TaskService taskService;
 
 	@Test
 	void contextLoads() {
