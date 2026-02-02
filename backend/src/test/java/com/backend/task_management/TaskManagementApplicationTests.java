767e4981b5971ffd69d018f6828c1ba03a864a27
Akmal <m.akmalalavi@gmail.com>
2026-02-02 13:25:51 +0530

    fix: remove environment variables from backend tests in CI/CD pipeline

--- backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
+++ backend/src/test/java/com/backend/task_management/TaskManagementApplicationTests.java
@@ -3,7 +3,9 @@ package com.backend.task_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class TaskManagementApplicationTests {
 
 	@Test
